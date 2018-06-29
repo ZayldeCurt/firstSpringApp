44b4d65d3709deab94e8d953bf15f9db55a8a7d0
ZayldeCurt <michaswiatowy@gmail.com>
2018-06-29 17:42:36 +0200

    add parse to book

--- src/main/java/com/pokemon/rest/BookClientRest.java
+++ src/main/java/com/pokemon/rest/BookClientRest.java
@@ -1,6 +1,9 @@
 package com.pokemon.rest;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pokemon.dto.BookUkLibDto;
 import com.pokemon.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +29,17 @@ public class BookClientRest {
         book.setContent(restTemplate.getForObject(
                 "http://bnb.data.bl.uk/doc/resource/007446989.json",String.class)); //biblioteka angielska
         JsonNode jsonNode = bookService.returnParsedBookContent(book);
-        return "here will be our pokemon";
+//        return "here will be our pokemon";
+        return prettyJsonString(jsonNode);
+    }
+
+    private String prettyJsonString(JsonNode jsonNode) {
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            Object json = mapper.readValue(jsonNode.toString(), Object.class);
+            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
+        } catch (Exception e) {
+            return "cannot parse";
+        }
     }
 }
