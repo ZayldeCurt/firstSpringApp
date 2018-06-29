30864e80d750889a2da9aa1dbde68842affac6cd
ZayldeCurt <michaswiatowy@gmail.com>
2018-06-29 18:09:36 +0200

    add first end point for pokemon api

--- src/main/java/com/pokemon/rest/PokemonClientRest.java
+++ src/main/java/com/pokemon/rest/PokemonClientRest.java
@@ -1,8 +1,15 @@
 package com.pokemon.rest;
 
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,11 +27,28 @@ public class PokemonClientRest {
 
     @RequestMapping("/pokemon")//aby była widoczna jako usługa restowa, wystawia jako endpoint,
     // dzieki temu będzie ta funkcja widoczna z localhost:8080
-    public String getOurPokemon(){
+    public String getOurPokemon(@RequestParam(value = "id") String id){
         //todo client to pokemon api (https://www.pokeapi.co/)
         // to get information about Resource for bulbasaur
 
-        return "here will be our pokemon";
+        CloseableHttpClient httpClient
+                = HttpClients.custom()
+                .setSSLHostnameVerifier(new NoopHostnameVerifier())
+                .build();
+        HttpComponentsClientHttpRequestFactory requestFactory
+                = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+
+        ResponseEntity<String> response
+                = new RestTemplate(requestFactory).exchange(
+                "http://pokeapi.co/api/v2/pokemon/"+id +"/", HttpMethod.GET, null, String.class);
+//        return response.getBody();
+
+        String result = response.toString();
+        String pokemonName = result.split(",\"name\":\"")[1].split("\"}],")[0];
+
+        return pokemonName;
     }
+
 
 }
